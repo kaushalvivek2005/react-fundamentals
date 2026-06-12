a8e5143a878bc57867487dc2171f9a4a56298421
Kaushal-tech <kaushalvivek2005@gmail.com>
2026-06-12 09:44:58 +0000

    props are studied and customreact created

--- 1vitereact/src/App.jsx
+++ 1vitereact/src/App.jsx
@@ -3,14 +3,17 @@
 import Chai from "./chai"
 
 function App() {
-
+  const username = "kaushal kumar"
 
   return (
     <>      
     {/* fragment(<>...</>) */}
+    {/*we can only write evaluated expressions inside the curly braces, we cannot write statements like if else or for loop*/}
    <Chai/>
    <h1>chai is very hot</h1>
+   <h2>hello {username}</h2>    
    <p>test paragraph</p>
+
   </>
   )
 }
--- 1vitereact/src/main.jsx
+++ 1vitereact/src/main.jsx
@@ -1,10 +1,23 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+ const anotherUser = " kaushal"
+
+const reactElement = React.createElement( // if we have to use this then we have to import React in the file
+  'a',
+  { href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    reactElement
+  
 )
