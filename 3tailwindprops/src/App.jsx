7ea8030e1e89b5cb9442b1462e38fc2e15a22256
Kaushalvivek2005 <kaushalvivek2005@gmail.com>
2026-07-09 23:36:19 +0530

    tailwindprops done

--- 3tailwindprops/src/App.jsx
+++ 3tailwindprops/src/App.jsx
@@ -7,12 +7,18 @@ import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "kaushal",
+    age: 20
+  }
+  let myArr = [1, 2, 3]
 
   return (
     <>
       <h1 className='bg-green-500 text-black p-4 rounded-xl'>Tailwind Test</h1>
-      <Card/>
-      <Card/>
+      {/* <Card channel= "chai" someObj= {myObj} newArr = {myArr} /> */}
+      <Card username = "kaushal" btnTxt= "click me"/>
+      <Card username = "komal" btnTxt= "visit me"/>
     </>
   )
 }
--- 3tailwindprops/src/components/Card.jsx
+++ 3tailwindprops/src/components/Card.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-function Card() {
+function Card({username , btnTxt="buy now"}) {
+  // console.log("props", props);
+  console.log(username);
+  
+  
     return(
     <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
@@ -9,10 +13,11 @@ function Card() {
       alt="Shoes" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
+    {/* <h2 className="card-title">kamal</h2> */}
+    <h2 className="card-title">{username}</h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">{btnTxt}</button>
     </div>
   </div>
 </div>
