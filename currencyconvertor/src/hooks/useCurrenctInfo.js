b72dfe1829ff30daec9dde35e08b452a7494c984
Kaushalvivek2005 <kaushalvivek2005@gmail.com>
2026-07-15 01:39:23 +0530

    currency converter completed

--- currencyconvertor/src/hooks/useCurrenctInfo.js
+++ currencyconvertor/src/hooks/useCurrenctInfo.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(()=>{
-        fetch('https://latest.currency-api.pages.dev/v1/currencies/${currency}.json')
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
         .then((res)=> res.json())
         .then((res)=> setData(res[currency]))
     }, [currency])
